aa291a13c8fda2503bbcac144574558d68eb781d
Erin  Rose <errose10@gmail.com>
2020-01-06 16:35:19 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,5 +4,14 @@
 # or whitespace (' ').
 
 class EmailAddressParser
-  EmailParser = ()
+  attr_accessor :emails
+
+  def initialize(emails)
+    @emails = emails
+  end
+  
+  def parse
+    emails.split(/, | /).uniq
+  end
+    
 end
